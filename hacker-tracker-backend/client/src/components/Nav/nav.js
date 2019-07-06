d5c6b22b1689906a8ca2cfc73c376021cf209875
samlutz <sammlutz@gmail.com>
2019-07-05 22:47:23 -0400

    navbar edit

--- hacker-tracker-backend/client/src/components/Nav/nav.js
+++ hacker-tracker-backend/client/src/components/Nav/nav.js
@@ -6,6 +6,18 @@ function Nav() {
       <a className="navbar-brand" href="/">
         Hacker-Tracker: Personal Productivity Made Easy!
       </a>
+      <a className="navbar-brand" href="/">
+        Todo's
+      </a>
+      <a className="navbar-brand" href="/">
+        personal notes
+      </a>
+      <a className="navbar-brand" href="/">
+        Saved Articles/Videos
+      </a>
+      <a className="navbar-brand" href="/">
+        Hacker Time Tracker
+      </a>
     </nav>
   );
 }
