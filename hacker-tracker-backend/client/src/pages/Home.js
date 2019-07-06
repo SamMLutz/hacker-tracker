9dfa046fa3aada3124977dc4379e0baf8d4341ab
samlutz <sammlutz@gmail.com>
2019-07-06 17:33:51 -0400

    tentative home screen complete

--- hacker-tracker-backend/client/src/pages/Home.js
+++ hacker-tracker-backend/client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron/jumbo";
+import { Col, Row, Container } from "../components/Grid";
 
 class Home extends Component {
     state = {
@@ -12,6 +13,30 @@ class Home extends Component {
                 <Jumbotron>
                     <h1>Welcome to Hacker Tracker!</h1>
                 </Jumbotron>
+                <Row>
+                    <Col size="md-6">
+                        <Jumbotron>
+                            <h1>Track your coding time with our Hacker Time Tracker!</h1>
+                        </Jumbotron>
+                    </Col>
+                    <Col size="md-6">
+                        <Jumbotron>
+                            <h1>Build a personal resource of saved articles and videos!</h1>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-6">
+                        <Jumbotron>
+                            <h1>Keep track of all your Goals and Todo's</h1>
+                        </Jumbotron>
+                    </Col>
+                    <Col size="md-6">
+                        <Jumbotron>
+                            <h1>Keep personal notes using our Notepad</h1>
+                        </Jumbotron>
+                    </Col>
+                </Row>
             </div>
         )
     }
